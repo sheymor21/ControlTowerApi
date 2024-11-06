da98e12cc9c64068141b45bf174e047c00bda319
Sheymor <joseacvz81@gmail.com>
2024-11-06 16:29:20 -0400

    fix: setter for passengerId is now accesible

--- src/main/java/com/sheymor/controltower/Entities/Passenger.java
+++ src/main/java/com/sheymor/controltower/Entities/Passenger.java
@@ -1,7 +1,6 @@
 package com.sheymor.controltower.Entities;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +14,6 @@ public class Passenger {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Setter(AccessLevel.NONE)
     private String passengerId;
     private String firstName;
     private String lastName;
