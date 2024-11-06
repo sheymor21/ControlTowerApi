c14e4249b3b679a5338b3a2a0bc4ba9aa2242192
Sheymor <joseacvz81@gmail.com>
2024-11-06 16:25:49 -0400

    fix: AirplaneAddDTO code now has the correct validation for code

--- src/main/java/com/sheymor/controltower/Dto/Airplane/AirplaneAddDTO.java
+++ src/main/java/com/sheymor/controltower/Dto/Airplane/AirplaneAddDTO.java
@@ -1,10 +1,10 @@
 package com.sheymor.controltower.Dto.Airplane;
 
-import com.sheymor.controltower.Validations.Customs.ValidAirportCodeNotPresent;
+import com.sheymor.controltower.Validations.Customs.ValidAirplaneCodeNotPresent;
 import com.sheymor.controltower.Validations.Customs.ValidAirportCodePresent;
 
 public record AirplaneAddDTO(
-        @ValidAirportCodeNotPresent String code,
+        @ValidAirplaneCodeNotPresent String code,
         @ValidAirportCodePresent String airportCode,
         String model,
         String company,
