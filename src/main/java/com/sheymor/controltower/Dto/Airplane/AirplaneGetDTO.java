0c723f96d1f8352495fbd329a0533d7c72a43109
Sheymor <joseacvz81@gmail.com>
2024-11-09 11:28:49 -0400

    Remove Jackson annotations from DTO classes
    Removed @JsonInclude and @JsonAutoDetect annotations from FlightsGetDTO and AirplaneGetDTO to simplify the DTOs and rely on default Jackson behavior. This change aims to reduce unnecessary configurations and streamline the data serialization process.

--- src/main/java/com/sheymor/controltower/Dto/Airplane/AirplaneGetDTO.java
+++ src/main/java/com/sheymor/controltower/Dto/Airplane/AirplaneGetDTO.java
@@ -1,13 +1,8 @@
 package com.sheymor.controltower.Dto.Airplane;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 /**
  * DTO for {@link com.sheymor.controltower.Entities.Airplane}
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record AirplaneGetDTO(
         String code,
         String model,
--- src/main/java/com/sheymor/controltower/Dto/Flight/FlightsGetDTO.java
+++ src/main/java/com/sheymor/controltower/Dto/Flight/FlightsGetDTO.java
@@ -1,15 +1,11 @@
 package com.sheymor.controltower.Dto.Flight;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Date;
 
 /**
  * DTO for {@link com.sheymor.controltower.Entities.Flight}
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record FlightsGetDTO(
         String flightId,
         String airplaneCode,
